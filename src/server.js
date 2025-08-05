eb7f0d4b1472f12336bcb2db29ea0004a26a3f7b
VyshnevskaO <vyshnevskaolenka@gmail.com>
2025-08-05 18:33:36 +0200

    init commit for render

--- src/server.js
+++ src/server.js
@@ -28,7 +28,8 @@ export const setupServer = () => {
   });
 
   app.get('/contacts/:contactId', async (req, res) => {
-    const contact = await getContactById();
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
     if (!contact) {
       res.status(404).json({
         message: 'Contact not found',

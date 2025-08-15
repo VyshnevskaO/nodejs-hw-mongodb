94a9e33ba06829d433d31098782914578e19e965
VyshnevskaO <vyshnevskaolenka@gmail.com>
2025-08-15 23:32:18 +0200

    fixed errorhandler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,26 +1,17 @@
+import { HttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: err,
+    });
+    return;
+  }
   res.status(500).json({
     status: 500,
     message: 'Something went wrong',
     data: err.message,
   });
-  return;
 };
-//  Second Option?(not according to TechTask, but from lection material)
-// import { HttpError } from 'http-errors';
-
-// export const errorHandler = (err, req, res, next) => {
-//   if (err instanceof HttpError) {
-//     res.status(err.status).json({
-//       status: err.status,
-//       message: err.name,
-//       data: err.message,
-//     });
-//     return;
-//   }
-//   res.status(500).json({
-//     status: 500,
-//     message: 'Something went wrong',
-//     data: err,
-//   });
-// };

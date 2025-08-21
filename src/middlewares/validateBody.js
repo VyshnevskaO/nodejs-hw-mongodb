352ea0cde3d341ebeb39761c7823f748cdffe62f
VyshnevskaO <vyshnevskaolenka@gmail.com>
2025-08-21 23:27:21 +0200

    fixed error messg and Joi schema

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -7,8 +7,10 @@ export const validateBody = (schema) => async (req, res, next) => {
     });
     next();
   } catch (err) {
-    const error = createHttpError(400, 'Bad Request', {
-      errors: err.detailes,
+    const errorMessages = err.details.map((detail) => detail.message);
+    const error = createHttpError(400, {
+      message: 'Validation error',
+      errors: errorMessages,
     });
     next(error);
   }

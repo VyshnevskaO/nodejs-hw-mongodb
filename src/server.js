14a6ef9df44946f28693d9de2b9e5044beb20baf
VyshnevskaO <vyshnevskaolenka@gmail.com>
2025-09-13 22:26:54 +0200

    Iniy commit

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { router } from './routers/index.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -27,9 +28,9 @@ export const setupServer = () => {
 
   app.use(router);
   app.use(errorHandler);
-
   app.use(notFoundHandler);
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
